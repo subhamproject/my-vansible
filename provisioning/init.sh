ff56d9c5ddadb2e37c9eb43893614df0fddb829c
subhamproject <subhammandal01@gmail.com>
2018-10-25 17:17:11 +0530

    Update init.sh

--- provisioning/init.sh
+++ provisioning/init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sudo apt-get -yq install software-properties-common
 sudo apt-add-repository -y ppa:ansible/ansible
-sudo apt-get -yq update
+#sudo apt-get -yq update
 sudo apt-get -yq install ansible
